16056ead6404177e3dcd5cdfe733f2dd45a475de
odumade <104792577+odumade@users.noreply.github.com>
2022-07-30 13:04:41 +0100

    complete the dot scroll with text

--- screens/Walkthrough/Walkthrough.js
+++ screens/Walkthrough/Walkthrough.js
@@ -12,6 +12,63 @@ const Walkthrough = () => {
 
     const scrollX = React.useRef(new Animated.Value(0)).current;
 
+    const Dots = () => {
+        const dotsPosition = Animated.divide(scrollX, SIZES.width)
+
+        return (
+            <View
+                style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+            >
+                {
+                    constants.walkthrough.map((item, index) => {
+                        const dotColor = dotsPosition.interpolate({
+                            inputRange: [index - 1, index, index + 1],
+                            outputRange: [COLORS.dark08, COLORS.primary, COLORS.dark08],
+                            extrapolate: "clamp"
+                        })
+                        return (
+                            <Animated.View
+                                key={`dot-${index}`}
+                                style={{
+                                    borderRadius: 5,
+                                    marginHorizontal: 6,
+                                    width: 10,
+                                    height: 10,
+                                    backgroundColor: dotColor
+                                }}
+                            />
+                        )
+                    })
+                }
+
+            </View>
+        )
+    }
+
+    function renderFooter() {
+        return (
+            <View
+                style={{
+                    position: 'absolute',
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    height: SIZES.height * 0.2,
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    paddingHorizontal: SIZES.padding,
+                    paddingVertical: SIZES.height > 700 ? SIZES.padding : 20
+                }}
+            >
+                <Dots />
+            </View>
+        )
+    }
+
     return (
         <View
             style={{
@@ -82,6 +139,8 @@ const Walkthrough = () => {
                     )
                 }}
             />
+
+            {renderFooter()}
 
         </View>
     )
